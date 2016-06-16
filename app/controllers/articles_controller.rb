220bd70fc42ccc11a73f921a02835ec37a533b17
dungphanx <dungphanx@gmail.com>
2016-06-17 01:09:25 +0700

    Add files via upload

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,6 +7,11 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all.order('created_at DESC')
     @markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+    if params[:search]
+      @articles = Article.search(params[:search]).order("created_at DESC")
+    else
+      @articles = Article.all.order("created_at DESC")
+    end
   end
 
   # GET /articles/1
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,10 @@
 class Article < ActiveRecord::Base
+	has_many :comments, dependent: :destroy
+	validates :title, presence: true,
+					  length: {minimum: 5 }
+
+	def self.search(search)
+  		#search_condition = "%" + search + "%"
+  		where("title LIKE ? OR body LIKE ?", "%#{search}%", "%#{search}%")
+	end
 end
